dc1d403e8f60fe5b6a2c2375f56a5dacef7fc641
Andy Walter <musictopia2@hotmail.com>
2023-06-02 20:39:20 -0700

    Added new property for techvaluepairmodel to allow the possibility that any automation that happens would exclude that item.

--- Models/TechValuePairModel.cs
+++ Models/TechValuePairModel.cs
@@ -3,4 +3,5 @@ public class TechValuePairModel
 {
     public string OriginalValue { get; set; } = "";
     public string ModifiedValue { get; set; } = "";
+    public bool ExcludeFromAutomation { get; set; } //now if marked true, then automation can't help  used for cases especially for low build limits
 }
