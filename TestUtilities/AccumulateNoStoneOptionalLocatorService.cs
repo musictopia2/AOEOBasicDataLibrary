46ce9c9bc38b254e3c43ecc4b19d44172e606cb2
Andy Walter <musictopia2@hotmail.com>
2023-06-21 17:33:46 -0700

    New quest for testing which has accumlate no stone optional. This means any custom techs for this quest must eliminate the stone part.

---
+++ TestUtilities/AccumulateNoStoneOptionalLocatorService.cs
@@ -0,0 +1,6 @@
+﻿namespace AOEOBasicDataLibrary.TestUtilities;
+public class AccumulateNoStoneOptionalLocatorService : IQuestLocatorService
+{
+    string IQuestLocatorService.OldQuestPath => Path.Combine(dd1.NewQuestFileDirectory, "Argos_S43_Laconia_Bandits_Legendary.quest");
+    string IQuestLocatorService.OldQuestTitle => "Sample Breaking Bandits";
+}
