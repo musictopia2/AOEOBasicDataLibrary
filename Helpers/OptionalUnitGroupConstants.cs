f92839ca5b5533e8d95b5f72bba48d4eed8db1dd
Andy Walter <musictopia2@hotmail.com>
2026-03-31 09:15:04 -0700

    Added seasonal unit constants and optional unit group constants.

---
+++ Helpers/OptionalUnitGroupConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace AOEOBasicDataLibrary.Helpers;
+public static class OptionalUnitGroupConstants
+{
+    /// <summary>
+    /// Represents priests that can convert other units.
+    /// Used for optionals that block training converting priests that become required offline when used.
+    /// </summary>
+    public const string ConvertingPriest = "ConvertingPriest";
+}
---
+++ Helpers/SeasonalUnitConstants.cs
@@ -0,0 +1,5 @@
+﻿namespace AOEOBasicDataLibrary.Helpers;
+public static class SeasonalUnitConstants
+{
+    public const string AbominableSnowman = "NPC_Winter_Unit_AbominableSnowmanFighter";
+}
